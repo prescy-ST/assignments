a44926b1de6d1e20b2d3fc43b6250be6e1c1a792
prescy-ST <santhosh.prescilla@gmail.com>
2026-05-31 20:58:04 +0530

    Add files via upload

---
+++ StringData.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class StringData {
+
+	public static void main(String[] args) {
+		
+		String name = "Joshua";
+		String course = "ST";
+			   System.out.println("Name: "+ name+"\n"+ "Course: " +course); //concatenate
+		String num1 = "10";
+		String num2 = "20";
+		       System.out.println(num1+num2);
+
+	}
+
+}
