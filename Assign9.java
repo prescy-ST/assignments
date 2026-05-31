fb4b66f233686adcd388730d136b86239e679b0c
prescy-ST <santhosh.prescilla@gmail.com>
2026-05-31 21:41:09 +0530

    Add files via upload

---
+++ Assign9.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Assign9 {
+
+	public static void main(String[] args) {
+		Map<String, Integer> stdlist = new TreeMap<>();
+		stdlist.put("joshua", 80);
+		stdlist.put("santhosh", 85);
+		stdlist.put("olive", 90);
+		stdlist.put("jasper", 95);
+		System.out.println(stdlist);
+	}
+
+}
